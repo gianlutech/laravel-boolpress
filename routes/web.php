42157c6fe29bc688bd0fec30649bb5f53aa041f1
gianlutech <gianluca.ingrao95@gmail.com>
2022-04-08 15:56:26 +0200

    create api postController

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,11 @@ Route::middleware('auth')
         })->where('any', '.*');    
     });
 
-    Route::get('{any?}', function () {
+    /* Route::get('{any?}', function () {
         return view('guest.home');
-    })->where("any", ".*");
+    })->where("any", ".*"); */
+
+    Route::get('/', function(){
+        return view('guest.home');
+    });
 
